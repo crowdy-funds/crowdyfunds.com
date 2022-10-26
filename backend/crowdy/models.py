6e67ab9fabb0a89ea3c7e6ce35bbd2835ede4b30
Robert Rakhmatullin <robertbg4@gmail.com>
2022-10-26 18:17:03 +0300

    company model added

--- backend/crowdy/models.py
+++ backend/crowdy/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+from typing import Final
 
-# Create your models here.
+from django.db import models
+from django.utils.translation.trans_null import gettext_lazy as _
+
+_COMPANY_NAME_MAX_LENGTH: Final = 150
+
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/company_<name>/<filename>
+    return f'company_{instance.name.replace(" ", "_")}/{filename.replace(" ", "_")}'
+
+
+class Company(models.Model):
+
+    name = models.CharField(_('name'), max_length=_COMPANY_NAME_MAX_LENGTH)
+    description = models.TextField(_('description'))
+    file = models.FileField(upload_to=user_directory_path)
+    picture = models.ImageField(upload_to=user_directory_path)
+
+    def __str__(self):
+        """Returns company name."""
+        return self.name
